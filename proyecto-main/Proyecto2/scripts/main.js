8bd5b4991b4f20f0b92edd951d1a2d8e16ca2507
MrM333 <109540206+MrM333@users.noreply.github.com>
2022-09-26 10:57:06 -0500

    Add files via upload

---
+++ proyecto-main/Proyecto2/scripts/main.js
@@ -0,0 +1,18 @@
+function prueba(){
+    
+    let imagen = document.getElementById("imgCentral");
+    console.log(imagen);
+    imagen.setAttribute("src","./images/Home6.png");
+}
+function prueba1(){
+    
+    let imagen = document.getElementById("imgCentral");
+    console.log(imagen);
+    imagen.setAttribute("src","./images/Home11.png");
+}
+function prueba2(){
+    
+    let imagen = document.getElementById("imgCentral");
+    console.log(imagen);
+    imagen.setAttribute("src","./images/Home7.png");
+}
